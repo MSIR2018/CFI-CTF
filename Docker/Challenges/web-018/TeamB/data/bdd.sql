cf38f948d6798dc29864879c9afa201956c74eb6
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-02-23 11:54:29 +0100

    a

--- Docker/Challenges/web-018/TeamB/data/bdd.sql
+++ Docker/Challenges/web-018/TeamB/data/bdd.sql
@@ -1,6 +1,6 @@
-drop database if exists flag_team_B;
-create database flag_team_B;
-use flag_team_B;
+drop database if exists flag_team_A;
+create database flag_team_A;
+use flag_team_A;
 
 create table utilisateurs (
 id_user int (3) not null auto_increment,
@@ -8,4 +8,4 @@ login varchar (20),
 password varchar(20),
 Primary Key (id_user));
 
-insert into utilisateurs values (,'user','@dm1n1str4tor');
+insert into utilisateurs (login,password) values ('user','4dm1n1str@tor');
