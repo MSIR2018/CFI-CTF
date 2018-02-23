808e20d0344a8a8fde34e6ae88f58a3ed03bc135
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-02-23 10:37:50 +0100

    dz
    dz

--- template_a_traiter/Faille FTP/TEAM A/data/index.php
+++
@@ -1,3 +0,0 @@
-<html>
-<body><a href="team_a.pcap" download="team_a.pcap" target="_blank" class="et_pb_button">team_a.pcap</a></body>
-</html>
--- template_a_traiter/Faille FTP/TEAM B/data/index.php
+++
@@ -1,3 +0,0 @@
-<html>
-<body><a href="team_b.pcap" download="team_b.pcap" target="_blank" class="et_pb_button">team_b.pcap</a></body>
-</html>
