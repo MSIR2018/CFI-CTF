83042eb3fff779b4ccdad6968f543ac612d61266
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-02-26 14:39:54 +0100

    d

--- template_a_traiter/Failles Web/Team B/data/index.php
+++
@@ -1,9 +0,0 @@
-<html>
-
-<head>
-type="text/javascript">
-	var pass = unescape("unescape%28%22String.fromCharCode%2528104%242C78%252C117%252C102%252C106%252C100%252C107%252C105%252C49%252C53%252C54%2529%22%29");
-</script>
-</head>
-
-</html>
