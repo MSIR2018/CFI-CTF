f9baafc74e8f0b66545f7831b9be82babf5e8a64
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-02-26 16:22:49 +0100

    Add files via upload

---
+++ Docker/Challenges/cryptanalyse-020/TeamA/data/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+
+<html lang="en" >
+
+<head>
+    <meta charset="UTF-8">
+    <title>Android Pattern</title>
+
+
+    <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900'>
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css'>
+    <link rel='stylesheet prefetch' href='https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css'>
+    <link rel='stylesheet prefetch' href='https://github.com/khadkamhn/lock-screen/raw/master/patt/pattern-lock.min.css'>
+
+    <link rel="stylesheet" href="css/style.css">
+
+
+</head>
+
+<body>
+
+
+
+<div class="mhn-ui-wrap">
+    <div class="mhn-ui-page page-lock">
+        <div class="mhn-ui-date-time">
+            <div class="mhn-ui-time"></div>
+            <div class="mhn-ui-day"></div>
+            <div class="mhn-ui-date"></div>
+        </div>
+        <div class="mhn-lock-wrap">
+            <div class="mhn-lock-title" data-title="Dessiner un schéma pour dévérouiller"></div>
+            <div class="mhn-lock"></div>
+        </div>
+    </div>
+</div>
+<div class="mhn-ui-info">Trouver le bon schéma pour pouvoir déverrouiller le téléphone ;)</div>
+
+<div class="mhn-ui-info"><a href="android_a.zip" target="_blank" style="color: lawngreen; font-size: 34px" download>Télécharger ici le dossier des fichiers source android</a></div>
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+
+
+<script  src="js/index.js"></script>
+
+
+</body>
+
+</html>
