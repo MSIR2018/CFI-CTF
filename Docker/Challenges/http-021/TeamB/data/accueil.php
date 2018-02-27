f08d8224149b361e1b7943da0e4924972b073f04
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-02-27 13:12:27 +0100

    Update accueil.php

--- Docker/Challenges/http-021/TeamB/data/accueil.php
+++ Docker/Challenges/http-021/TeamB/data/accueil.php
@@ -7,5 +7,9 @@
         <h1>Bienvenue sur mon site !</h1>
         <p>Site en construction...</p>
     </center>
+    Reste à faire :
+    <li>Finir la page de connexion à la bdd</li>
+    <li>Faire une page profil utilisateur</li>
+    <li>Mettre du CSS (bootsrap)</li>
 </div>
 </html>
