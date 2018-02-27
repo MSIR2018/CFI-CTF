f5f44c4c25ac13f677ba4f316338873ae1987398
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-02-27 16:04:41 +0100

    d

---
+++ Docker/Challenges/system-030/TeamB/data/index.php
@@ -0,0 +1,40 @@
+<script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<head>
+
+    <title>Fichiers</title>
+
+</head>
+
+
+<div class="container">
+    <h2>Fichiers</h2>
+    <hr>
+    <div class="row">
+        <div class="col-xs-12" style="">
+            <div class="panel panel-default list-group-panel">
+                <div class="panel-body">
+                    <ul class="list-group list-group-header">
+                        <li class="list-group-item list-group-body">
+                            <div class="row">
+                                <div class="col-xs-6 text-left">Nom</div>
+                                <div class="col-xs-3">Taille</div>
+                                <div class="col-xs-3">Modifié le</div>
+                            </div>
+                        </li>
+                    </ul>
+                    <ul class="list-group list-group-body" style="">
+                        <li class="list-group-item">
+                            <div class="row">
+                                <div class="col-xs-6 text-left" style=" "> <a href="capt" download><span class="glyphicon glyphicon-file" aria-hidden="true"></span> Capture Ethernet </a> </div>
+                                <div class="col-xs-3" style="">8 KB</div>
+                                <div class="col-xs-3" style="">22/02/2018 09:53:06</div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
