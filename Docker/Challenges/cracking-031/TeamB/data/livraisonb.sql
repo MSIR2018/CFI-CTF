4706f25b41cd7827e8959eb732b93123cf878821
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-02-27 16:44:25 +0100

    Add files via upload
    upload all files cracking031

---
+++ Docker/Challenges/cracking-031/TeamB/data/livraisonb.sql
@@ -0,0 +1,167 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Mar 27 Février 2018 à 15:58
+-- Version du serveur :  5.7.21-0ubuntu0.16.04.1
+-- Version de PHP :  7.0.25-0ubuntu0.16.04.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `Livraison`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `boisson`
+--
+
+CREATE TABLE `boisson` (
+  `id` int(11) NOT NULL,
+  `name_boisson` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `boisson`
+--
+
+INSERT INTO `boisson` (`id`, `name_boisson`) VALUES
+(1, 'Ice Tea'),
+(2, 'Coca'),
+(3, 'Coca Zero'),
+(4, 'Coca Light'),
+(5, 'Fanta'),
+(6, 'Orangina'),
+(7, 'Docteur Pepper'),
+(8, 'Oasis'),
+(9, 'Tropico'),
+(10, 'Sprite');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dessert`
+--
+
+CREATE TABLE `dessert` (
+  `id` int(11) NOT NULL,
+  `name_dessert` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `dessert`
+--
+
+INSERT INTO `dessert` (`id`, `name_dessert`) VALUES
+(1, 'Gland'),
+(2, 'Flan'),
+(3, 'Paint au chocolat'),
+(4, 'Croissant'),
+(5, 'Tarte Framboise'),
+(6, 'Chouqette'),
+(7, 'Eclair'),
+(8, 'Tiramisu'),
+(9, 'Tarte aux Daims'),
+(10, 'Religieuse');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `flag`
+--
+
+CREATE TABLE `flag` (
+  `id` int(11) NOT NULL,
+  `flag` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `flag`
+--
+
+INSERT INTO `flag` (`id`, `flag`) VALUES
+(1, 'uncocazerosvp');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `plat`
+--
+
+CREATE TABLE `plat` (
+  `id` int(11) NOT NULL,
+  `name_plat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `plat`
+--
+
+INSERT INTO `plat` (`id`, `name_plat`) VALUES
+(1, 'Salade Tomage Oignon');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `boisson`
+--
+ALTER TABLE `boisson`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `dessert`
+--
+ALTER TABLE `dessert`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `flag`
+--
+ALTER TABLE `flag`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `plat`
+--
+ALTER TABLE `plat`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `boisson`
+--
+ALTER TABLE `boisson`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT pour la table `dessert`
+--
+ALTER TABLE `dessert`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+--
+-- AUTO_INCREMENT pour la table `flag`
+--
+ALTER TABLE `flag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT pour la table `plat`
+--
+ALTER TABLE `plat`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
