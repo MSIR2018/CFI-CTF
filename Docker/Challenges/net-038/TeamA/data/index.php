11a775aee2c375dd31a45720ee2a9d873c470a3a
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-03-06 16:05:06 +0100

    Create index.php

---
+++ Docker/Challenges/net-038/TeamA/data/index.php
@@ -0,0 +1,10 @@
+<html>
+<center>
+<h2>Disponibilite de service</h2>
+<p>L'objectif est simple: rendez indisponible ce serveur et le flag apparaitera sur cette Page
+</br>
+</br>
+Attention pensez bien au type d'attaque, le but n'est pas de le rendre hors service definitivement mais surcharger sa bande passante..</p>
+Bande passante actuelle: <?php echo shell_exec('bash current.sh'); ?> kb/s
+</center>
+<html>
