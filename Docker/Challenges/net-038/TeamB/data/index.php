24fddd28270826413f5c12e512a9e0721d990f4b
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-03-06 17:24:42 +0100

    Update and rename index.html to index.php

--- Docker/Challenges/net-038/TeamB/data/index.php
+++ Docker/Challenges/net-038/TeamB/data/index.php
@@ -1,9 +1,11 @@
 <html>
 <center>
 <h2>Disponibilite de service</h2>
-<p>L'objectif est simple: rendez indisponible ce serveur et le flag apparaitera sur cette Page 
+<p>L'objectif est simple: rendez indisponible ce serveur et le flag apparaitera sur cette Page
 </br>
 </br>
 Attention pensez bien au type d'attaque, le but n'est pas de le rendre hors service definitivement mais surcharger sa bande passante..</p>
+Bande passante actuelle: <?php echo shell_exec('bash current.sh'); ?> kb/s </br>
+Objectif de surcharge: 10 mb/s
 </center>
 <html>
