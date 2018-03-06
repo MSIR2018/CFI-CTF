74f5389c591e533d376c2d31ecdaa4c4645a3a66
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-03-06 15:48:56 +0100

    Add files via upload

---
+++ Docker/Challenges/net-038/TeamB/data/ddos-check.sh
@@ -0,0 +1,13 @@
+#while true ; do
+a=`cat /sys/class/net/eth0/statistics/rx_bytes`
+sleep 1
+b=`cat /sys/class/net/eth0/statistics/rx_bytes`
+by=$((b-a))
+kb=$((by/1024))
+mb=$((kb/1024))
+
+if [ $mb -gt 8 ];then
+echo $( echo "scale=3; $kb/1024" | bc );
+fi
+
+#done
