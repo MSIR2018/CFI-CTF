3534e2287847995528ae785c240b1d5c9672f25f
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-03-07 10:02:39 +0100

    Update ddos-check.sh

--- Docker/Challenges/net-038/TeamA/data/ddos-check.sh
+++ Docker/Challenges/net-038/TeamA/data/ddos-check.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
-a=$(cat /sys/class/net/eth0/statistics/rx_bytes)
+a=`cat /sys/class/net/eth0/statistics/rx_bytes`
 sleep 1
-b=$(cat /sys/class/net/eth0/statistics/rx_bytes)
+b=`cat /sys/class/net/eth0/statistics/rx_bytes`
 by=$((b-a))
 kb=$((by/1024))
-mb=$((kb/1024))
-target=4
-if [ $mb -gt $target ];then
-echo $mb
+target=2000
+echo $kb
+
+if [ $kb -gt $target ];then
+echo 'ok'
 fi
