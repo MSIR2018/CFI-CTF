45e02ee9e9af168cb927ae88247570a4699649ad
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-03-07 09:55:53 +0100

    Update ddos-check.sh

--- Docker/Challenges/net-038/TeamA/data/ddos-check.sh
+++ Docker/Challenges/net-038/TeamA/data/ddos-check.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-#while true ; do
-a=`cat /sys/class/net/eth0/statistics/rx_bytes`
-
-sleep 1;
-b=`cat /sys/class/net/eth0/statistics/rx_bytes`
+a=$(cat /sys/class/net/eth0/statistics/rx_bytes)
+sleep 1
+b=$(cat /sys/class/net/eth0/statistics/rx_bytes)
 by=$((b-a))
 kb=$((by/1024))
 mb=$((kb/1024))
 target=4
-
 if [ $mb -gt $target ];then
 echo $mb
 fi
-
-#done
