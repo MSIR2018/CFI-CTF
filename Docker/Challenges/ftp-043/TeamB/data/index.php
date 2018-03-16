4524043343f6360b7f67cee4e8cb6b2a1553a255
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-03-16 11:34:34 +0100

    Add files via upload

---
+++ Docker/Challenges/ftp-043/TeamB/data/index.php
@@ -0,0 +1,30 @@
+<?php
+
+if(isset($_POST['valider']))
+
+{	
+		if(($ftp = ftp_connect("localhost",21))==false)
+		{
+			echo 'erreur de connexion';
+		}
+		if(!ftp_login($ftp,"tom","duselpourlesfrites"))
+		{
+			echo 'l\'identification a échoué...';
+		}
+		else
+		{
+			echo 'connexion FTP ok';
+		}
+}
+?>
+
+<html>
+<head>
+
+</head>
+<body>
+		<form method="post" action="index.php">
+		<input type="submit" value="Se Connecter" name="valider">
+	</form>
+</body>
+</html>
