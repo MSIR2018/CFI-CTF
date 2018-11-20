3451b43209206cb94ef4bac6b85f72f84ed58f74
MSIR2018 <36412637+MSIR2018@users.noreply.github.com>
2018-11-20 22:07:28 +0100

    Update backupsql.sql

--- Docker/Host/Data/backupsql.sql
+++ Docker/Host/Data/backupsql.sql
@@ -19,8 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `CTF`
 --
-DROP DATABASE IF EXISTS CTF
-CREATE DATABASE `CTF` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+DROP DATABASE IF EXISTS CTF;
+CREATE DATABASE `CTF`;
 USE `CTF`;
 
 -- --------------------------------------------------------
